efbb98589462e58bf8e2441803917c7cc55d8121
José Luis Lobera <40039535+JHludwolf@users.noreply.github.com>
2023-05-19 10:00:35 -0600

    Route to Transfer page

--- EasyPAI_front/src/App.jsx
+++ EasyPAI_front/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
+import Transfer from './pages/transfer'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path='/transfer' element={<Transfer />} />
       </Routes>
     </BrowserRouter>
   )
