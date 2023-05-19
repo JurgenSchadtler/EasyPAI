bef4baab769fb3f4b2d1c687674969a1e8c9a0ce
JurgenSZ <jschadtlerz@gmail.com>
2023-05-19 12:37:54 -0600

    Update expenses.jsx

--- EasyPAI_front/src/pages/expenses.jsx
+++ EasyPAI_front/src/pages/expenses.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout2 from "../components/layout2";
-
+import axios from "axios";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -11,22 +11,22 @@ import "../style/expenses.css";
 
 
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1namdsa2xkeHpnbnRqbGRtcGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1MTUzOTMsImV4cCI6MjAwMDA5MTM5M30.12PsI2OKWJVKXOACa4dXV6jU-nAO8QUVDKooqnjQ1Xc';
-
-const USER_URL = 'https://mgjglkldxzgntjldmpgn.supabase.co/rest/v1/user?select=*';
+const API_URL = 'https://mgjglkldxzgntjldmpgn.supabase.co/rest/v1/transfers?select=*';
 
 const Expenses = () => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(USER_URL, {
+        const response = await axios.get(API_URL, {
           headers: {
             apikey: API_KEY,
             Authorization: `Bearer ${API_KEY}`
           }
         });
 
-        setUser(response.data);
+        setData(response.data);
         console.log(response.data);
       } catch (error) {
         console.error('Error:', error);
@@ -43,24 +43,27 @@ const Expenses = () => {
 
       <Container fluid style={{ marginTop: "1rem" }} className="">
         <div className="expenses-full-div">
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((n) => (
-            <Row className="expenses-full-row" key={`key-${n}`}>
-              <Col xs={3}>
-                <div className="expenses-full-icon-div">
-                  <BsBank className="expenses-full-icon" />
-                </div>
-              </Col>
-              <Col xs={6} className="expenses-full-info-div">
-                <div>
-                  <p className="expenses-full-title">Grocery</p>
-                  <p className="expenses-full-date">18-05-2023</p>
-                </div>
-              </Col>
-              <Col xs={3} className="expenses-full-info-div">
-                <p className="expenses-full-amount">$25</p>
-              </Col>
-            </Row>
-          ))}
+          {data.map((item, index) => {
+            const trimmedDate = item.created_at.substring(2, 10).replace(/-/g, '/');
+            return (
+              <Row className="expenses-full-row" key={`key-${index}`}>
+                <Col xs={3}>
+                  <div className="expenses-full-icon-div">
+                    <BsBank className="expenses-full-icon" />
+                  </div>
+                </Col>
+                <Col xs={6} className="expenses-full-info-div">
+                  <div>
+                    <p className="expenses-full-title">{item.description}</p>
+                    <p className="expenses-full-date">{trimmedDate}</p>
+                  </div>
+                </Col>
+                <Col xs={3} className="expenses-full-info-div">
+                  <p className="expenses-full-amount">{item.ammount.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
+                </Col>
+              </Row>
+            );
+          })}
         </div>
       </Container>
     </Layout2>
