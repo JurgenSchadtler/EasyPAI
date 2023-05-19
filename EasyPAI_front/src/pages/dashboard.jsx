e69cf696554e21ba475ef2accbd82aa46c5ecc47
JurgenSZ <jschadtlerz@gmail.com>
2023-05-19 12:22:35 -0600

    Database connection

--- EasyPAI_front/src/pages/dashboard.jsx
+++ EasyPAI_front/src/pages/dashboard.jsx
@@ -1,6 +1,6 @@
 import Layout from "../components/layout";
 import "../style/dashboard.css";
-
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cards";
@@ -14,6 +14,13 @@ import Col from "react-bootstrap/Col";
 
 import { BsBank } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import { createClient } from '@supabase/supabase-js'
+
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1namdsa2xkeHpnbnRqbGRtcGduIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ1MTUzOTMsImV4cCI6MjAwMDA5MTM5M30.12PsI2OKWJVKXOACa4dXV6jU-nAO8QUVDKooqnjQ1Xc';
+const API_URL = 'https://mgjglkldxzgntjldmpgn.supabase.co/rest/v1/transfers?select=*';
+const USER_URL = 'https://mgjglkldxzgntjldmpgn.supabase.co/rest/v1/user?select=*';
 
 const Dashboard = () => {
   const color_palette = [
@@ -24,6 +31,9 @@ const Dashboard = () => {
     "#d8d3ff",
     "#f0efff",
   ];
+
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState([]);
 
   const saved_accounts = [
     "José Luis Lobera",
@@ -38,18 +48,61 @@ const Dashboard = () => {
 
   const onCardClick = (n) => {
     console.log(`Click on card no. ${n}`);
-	navigate('/transfer');
+    navigate('/transfer');
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API_URL, {
+          headers: {
+            apikey: API_KEY,
+            Authorization: `Bearer ${API_KEY}`
+          }
+        });
+
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(USER_URL, {
+          headers: {
+            apikey: API_KEY,
+            Authorization: `Bearer ${API_KEY}`
+          }
+        });
+
+        setUser(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+
+    fetchData();
+  }, []);
+
 
   return (
     <Layout>
       <div className="dashboard-div">
-        <h1 className="dashboard-h1">Jürgen</h1>
+        <h1 className="dashboard-h1">{user[0].name}</h1>
         <p className="dashboard-welcome-p">Welcome Back!</p>
 
         <div className="dashboard-balance-div">
           <p className="balance-tag">Current balance</p>
-          <p className="balance-value">$2,090.20</p>
+          <p className="balance-value">{user[0].balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</p>
         </div>
 
         <p className="dashboard-send-money-tag">Send money to</p>
@@ -107,24 +160,27 @@ const Dashboard = () => {
             </Col>
           </Row>
           <div className="expenses-div">
-            {[0, 1, 2, 3, 4, 5, 6].map((n) => (
-              <Row className="expenses-row" key={`key-${n}`}>
-                <Col xs={3}>
-                  <div className="expenses-icon-div">
-                    <BsBank className="expenses-icon" />
-                  </div>
-                </Col>
-                <Col xs={6} className="expenses-info-div">
-                  <div>
-                    <p className="expenses-title">Grocery</p>
-                    <p className="expenses-date">18-05-2023</p>
-                  </div>
-                </Col>
-                <Col xs={3} className="expenses-info-div">
-                  <p className="expenses-amount">$25</p>
-                </Col>
-              </Row>
-            ))}
+            {data.map((item, index) => {
+              const trimmedDate = item.created_at.substring(2, 10).replace(/-/g, '/');
+              return (
+                <Row className="expenses-row" key={`key-${index}`}>
+                  <Col xs={3}>
+                    <div className="expenses-icon-div">
+                      <BsBank className="expenses-icon" />
+                    </div>
+                  </Col>
+                  <Col xs={6} className="expenses-info-div">
+                    <div>
+                      <p className="expenses-title">{item.description}</p>
+                      <p className="expenses-date">{trimmedDate}</p>
+                    </div>
+                  </Col>
+                  <Col xs={3} className="expenses-info-div">
+                    <p className="expenses-amount">${item.ammount}</p>
+                  </Col>
+                </Row>
+              );
+            })}
           </div>
         </Container>
       </div>
